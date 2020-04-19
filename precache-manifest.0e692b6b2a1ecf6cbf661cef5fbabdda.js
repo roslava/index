fe2faa680157457814d04b11154c8e5cd597ec86
roslava <48380940+roslava@users.noreply.github.com>
2020-04-19 13:15:14 +0300

    Updates

---
+++ precache-manifest.0e692b6b2a1ecf6cbf661cef5fbabdda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f2cfbd92a677b08b20d19a05dfcc62",
+    "url": "/index/index.html"
+  },
+  {
+    "revision": "1768c2740bd61809cba8",
+    "url": "/index/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "95e23bda3b843d3232b1",
+    "url": "/index/static/js/2.2c0db735.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/index/static/js/2.2c0db735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1768c2740bd61809cba8",
+    "url": "/index/static/js/main.8bde118c.chunk.js"
+  },
+  {
+    "revision": "011f0e05f5099bd1a0a1",
+    "url": "/index/static/js/runtime-main.d9677299.js"
+  }
+]);
