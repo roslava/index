b02be0c52d538ffd05ffaefb78c434fbdd7e5941
roslava <48380940+roslava@users.noreply.github.com>
2020-04-19 14:25:30 +0300

    commit2

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import {Router, Route, Link} from "react-router-dom";
 function App() {
   return (
     <div>
-    <div><a target="_self" href="https://roslava.github.io/newspaper/">Газета №1</a></div>
-    <div><a target="_self" href="https://roslava.github.io/newspaper/">Газета №2</a></div>
-    <div><a target="_self" href="https://roslava.github.io/newspaper/">Газета №3</a></div>
+    <div><a target="_self" href="https://roslava.github.io/newspaper/articles">Газета №1</a></div>
+    <div><a target="_self" href="https://roslava.github.io/newspaper/articles">Газета №2</a></div>
+    <div><a target="_self" href="https://roslava.github.io/newspaper/articles">Газета №3</a></div>
 </div>
   );
 }
